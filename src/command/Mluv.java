5d0672d61e319cf700780f123f171ef7ca1a2045
marekneumann18 <fe@email.com>
2026-02-02 21:43:04 +0100

    upgrading prikay mluv

--- src/command/Mluv.java
+++ src/command/Mluv.java
@@ -3,7 +3,7 @@ package command;
 import game.GameData;
 import location.Location;
 import player.Player;
-import postavy.Character;
+import characters.Character;
 
 import java.util.Scanner;
 
@@ -37,6 +37,47 @@ public class Mluv extends Command {
                         String question = sc.next().toLowerCase();
                         if (ch.getDialogs().containsKey(question)) {
                             System.out.println(ch.getName() + ": " + ch.getDialogs().get(question));
+                            if (ch.getId().equalsIgnoreCase("captainamerica") && ch.getDialogs().containsKey("zbran")) {
+                                if (!l.getItems().contains("stit")) {
+                                    l.addItem("stit");
+                                    System.out.println("[INFO]: Captain America položil na zem svůj starý štít. Teď ho můžeš sebrat.");
+                                }
+
+                            } else if (ch.getId().equalsIgnoreCase("redskull") && ch.getDialogs().containsKey("hadanka")) {
+                                String answerMystery = sc.next().toLowerCase();
+                                if (answerMystery.equals("procesor ") || answerMystery.equals("cpu")) {
+                                    l.addItem("kamen duse");
+                                    System.out.println("Správně");
+                                    System.out.println("Kámen duše leží na zemi před tebou");
+
+                                }else{
+                                    System.out.println("špatná odpověď");
+                                }
+
+                            } else if (ch.getId().equalsIgnoreCase("blackpanther")  ) {
+                               if (ch.getDialogs().containsKey("brneni")) {
+                                   l.addItem("brneni");
+                               } else if (ch.getDialogs().containsKey("vibranium")) {
+                                   String answerMystery2 = sc.next().toLowerCase();
+                                   if (answerMystery2.equals("1")) {
+                                       l.addItem("thorovo kladivo");
+                                       System.out.println("Správně");
+                                       System.out.println("Thorovo kladivo zde leží");
+
+                                   }else{
+                                       System.out.println("špatná odpověď");
+                                   }
+                               }
+
+                                //TODO  obchodnik
+                            } else if (ch.getId().equalsIgnoreCase("obchodnik") && ch.getDialogs().containsKey("ukol")){
+                                if(player.getItems().contains("brneni")){
+
+
+                                }
+
+
+                            }
                         } else if (question.equalsIgnoreCase("konec")) {
                             answer = true;
 
@@ -58,32 +99,7 @@ public class Mluv extends Command {
 
     }
 
-    public void speakAvengerTower() {
-        Character ch = data.characters.get(1);
-        System.out.println("Mluvis s " + ch.getName());
-        System.out.println();
-        System.out.println();
-    }
 
-    public void speakWakanda() {
-        Character ch = data.characters.get(3);
-    }
-
-    public void speakAsgard() {
-        Character ch = data.characters.get(2);
-    }
-
-    public void speakNewYorkCity() {
-        Character ch = data.characters.get(0);
-    }
-
-    public void speakKnowhere() {
-        Character ch = data.characters.get(4);
-    }
-
-    public void speakVormir() {
-        Character ch = data.characters.get(6);
-    }
 
 
     @Override
