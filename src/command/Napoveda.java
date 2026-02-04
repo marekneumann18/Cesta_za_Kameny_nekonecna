242361db44bd1b213cc8c30434e59e8c6021b681
marekneumann18 <fe@email.com>
2026-02-04 22:17:20 +0100

    improving napoveda

--- src/command/Napoveda.java
+++ src/command/Napoveda.java
@@ -3,7 +3,7 @@ package command;
 public class Napoveda extends Command {
     @Override
     public String execute() {
-        return "Možné příkazy :\njdi {Asgard,New York City,Vormir,Avengers Tower,Titan,Knowhere,Wakanda,Sokovia}\nkonec hry\nmluv\nnapoveda\npouzij\nseber\nutok";
+        return "Všechno piš bez diakritiky\nMožné příkazy :\njdi {Asgard,New York City,Vormir,Avengers Tower,Titan,Knowhere,Wakanda,Sokovia}\nkonec hry\nmluv\nnapoveda\npouzij (např. stormbreaker nebo thorovo kladivo v lokaci Asgard, kod v Avengers Tower\nseber <predmet>\nutok";
     }
 
     @Override
