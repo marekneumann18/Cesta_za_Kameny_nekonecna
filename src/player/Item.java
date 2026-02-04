21ab7a9d8da97bc903531425b98cb4c97099f808
neumann <emal@s.com>
2026-02-04 11:13:45 +0100

    removing id because it was not needed

--- src/player/Item.java
+++ src/player/Item.java
@@ -1,7 +1,6 @@
 package player;
 
 public class Item {
-    private String id;
 
     private String name;
 
@@ -9,13 +8,7 @@ public class Item {
 
     }
 
-    public String getId() {
-        return id;
-    }
 
-    public void setString(String id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
@@ -28,7 +21,6 @@ public class Item {
     @Override
     public String toString() {
         return "Item{" +
-                "id=" + id +
                 ", nazev='" + name + '\'' +
                 '}';
     }
