24f57b5187b44100a0e92d7c50ed492769a58415
neumann <emal@s.com>
2026-02-04 11:14:34 +0100

    starting command pouzij

--- src/command/Pouzij.java
+++ src/command/Pouzij.java
@@ -1,11 +1,15 @@
 package command;
 
 import game.GameData;
+import location.Location;
 import player.Player;
+
+import java.util.Scanner;
 
 public class Pouzij extends Command {
     private Player player;
     private GameData data;
+    Scanner sc = new Scanner(System.in);
 
     public Pouzij(Player player, GameData data) {
         this.player = player;
@@ -14,6 +18,10 @@ public class Pouzij extends Command {
 
     @Override
     public String execute() {
+        String use = sc.next();
+        for (Location loc : data.getLocations()) {
+
+        }
         return "";
     }
 
