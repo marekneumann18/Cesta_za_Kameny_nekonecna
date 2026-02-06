c15d1d45fa2358810c35a42e475cf0b7e3f14586
neumann4 <mail@s.cz>
2026-02-06 10:58:02 +0100

    test

---
+++ java/player/PlayerTest.java
@@ -0,0 +1,16 @@
+package player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+}
